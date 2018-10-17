c74984f47ef0eba9f3dc99391cba7bda88eede81
snowsoft <abdulkadirkar@gmail.com>
2018-10-17 19:01:08 +0300

    -

--- src/MesajPaneli.php
+++ src/MesajPaneli.php
@@ -2,9 +2,9 @@
 
 namespace Nlksoft\SmsGateway;
 
-use Nlksoft\Exception\ClientException;
-use Nlksoft\Exception\SmsException;
-use Nlksoft\Exception\AuthenticationException;
+use Nlksoft\SmsGateway\Exception\ClientException;
+use Nlksoft\SmsGateway\Exception\SmsException;
+use Nlksoft\SmsGateway\Exception\AuthenticationException;
 
 
 class MesajPaneli {
